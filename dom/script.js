bb5cfb7ba0930dba12ccaa3458b1ec4d64317c81
Tarun S Maidur <tarunmaidur@gmail.com>
2024-03-04 13:02:47 +0530

    dom

---
+++ dom/script.js
@@ -0,0 +1,12 @@
+const btn = document.querySelector("button");
+
+btn.textContent = "Click me";
+let elem = document.getElementById("elem");
+
+let nav = document.getElementsByClassName("nav");
+
+console.log(btn);
+
+console.log(elem);
+
+console.log(nav);
